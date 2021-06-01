7b397d86aa2e6ce3b178629abdcd6001688c24bc
bq <email>
2021-06-01 11:28:26 +0800

    Initial commit

---
+++ src/main/java/com/eg/smarttranslate/SmartTranslateApplication.java
@@ -0,0 +1,13 @@
+package com.eg.smarttranslate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTranslateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTranslateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eg/smarttranslate/SmartTranslateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eg.smarttranslate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTranslateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
